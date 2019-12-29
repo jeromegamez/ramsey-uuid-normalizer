6a062a0235536854d340f26257eefe657d32417b
Jérôme Gamez <jerome@gamez.name>
2019-12-29 22:22:50 +0100

    Update PHPUnit

--- tests/Normalizer/UuidNormalizerTest.php
+++ tests/Normalizer/UuidNormalizerTest.php
@@ -9,31 +9,27 @@ use Ramsey\Uuid\UuidInterface;
 
 class UuidNormalizerTest extends TestCase
 {
-    /**
-     * @var UuidNormalizer
-     */
+    /** @var UuidNormalizer */
     private $normalizer;
 
-    /**
-     * @var Uuid
-     */
+    /** @var Uuid */
     private $uuid;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         $this->normalizer = new UuidNormalizer();
         $this->uuid = Uuid::uuid4();
     }
 
     /** @test */
-    public function it_normalizes_uuids()
+    public function it_normalizes_uuids(): void
     {
         $this->assertTrue($this->normalizer->supportsNormalization($this->uuid));
         $this->assertSame($this->uuid->toString(), $this->normalizer->normalize($this->uuid));
     }
 
     /** @test */
-    public function it_denormalizes_uuid_strings()
+    public function it_denormalizes_uuid_strings(): void
     {
         $this->assertTrue($this->normalizer->supportsDenormalization($this->uuid->toString(), Uuid::class));
         $this->assertTrue($this->normalizer->supportsDenormalization($this->uuid->toString(), UuidInterface::class));
